774b5d7e1ef38266b566c2b680f883d907bc5bca
Razvan Onofrei <razwan.me@gmail.com>
2013-09-17 17:11:40 +0300

    djax

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> data-nicescroll>
+<html class="djax-loading" <?php language_attributes(); ?> data-nicescroll>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
