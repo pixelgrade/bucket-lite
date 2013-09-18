9e348947881569e0056b9b01ba2344b0b8dd51c4
Razvan Onofrei <razwan.me@gmail.com>
2013-09-18 11:50:19 +0300

    a little bit of style in my menu

--- wpgrade-content/inc/menus.php
+++ wpgrade-content/inc/menus.php
@@ -71,6 +71,11 @@ function wpgrade_footer_nav() {
  */
 class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
 
+    function start_lvl(&$output, $depth = 0, $args = array()) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"site-navigation__sub-menu\">\n";
+    }
+
     function display_element($element, &$children_elements, $max_depth, $depth=0, $args, &$output) {
         $id_field = $this->db_fields['id'];
         if (!empty($children_elements[$element->$id_field])) {
@@ -87,8 +92,7 @@ class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
         // depth dependent classes
         $depth_classes = array(
             ( $depth == 0 ? 'menu-item--main' : 'menu-item--sub-menu-item' ),
-            ( $depth >=2 ? 'sub-sub-menu-item' : '' ),
-            ( $depth % 2 ? 'menu-item-odd' : 'menu-item-even' ),
+            ( $depth % 2 ? 'menu-item--odd' : 'menu-item--even' ),
             'menu-item-depth-' . $depth
         );
         $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
@@ -98,7 +102,7 @@ class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
         $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
       
         // build html
-        $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="site-navigation__menu-item  menu-item' . $depth_class_names . ' ' . $class_names . '">';
+        $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="site-navigation__menu-item  menu-item ' . $depth_class_names . ' ' . $class_names . '">';
       
         // link attributes
         $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
