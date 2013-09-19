2e98cd1346e8f474a7275e6895e87efab8330e42
Razvan Onofrei <razwan.me@gmail.com>
2013-09-19 17:41:57 +0300

    kill royal slider

--- theme-partials/post-templates/single-head.php
+++ theme-partials/post-templates/single-head.php
@@ -1,8 +1,3 @@
-<?php
-	global $post; // @todo CLEANUP verify this global call is required
-	$html_title = get_post_meta(get_the_ID(), wpgrade::prefix().'post_html_title', true);
-?>
-
 <?php if ( has_post_thumbnail() ): ?>
     <div class="featured-image">
         <?php the_post_thumbnail(); ?>
