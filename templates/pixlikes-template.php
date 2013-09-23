05c8c0692e6e3d4df8d2abdc9bcdbfd2a5e04054
cristianfrumusanu <cristian.frumusanu@gmail.com>
2013-09-23 11:16:55 +0300

    like animation

---
+++ templates/pixlikes-template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This is the default view for the like box
+ * It should be overridden by the theme but let's create an example
+ */
+
+/*
+ * There should be an element with the class="pixlikes-box"
+ * Also $data_id variable contains the post id required by the js file
+ */
+
+//echo '<a class="pixlikes-box '.$display_only.'" href="#" '.$data_id.' title="'. $title .'" style="background-color: #345; display:block; width:30px; height:30px">'. $likes_number .'</a>';?>
+<div class="pixlikes-box <?php echo $display_only . ' ' . $class ?> likes-box" <?php echo $data_id ?>>
+	<a class="like-link" href="#"><i class="icon-heart"></i></a>
+	<div class="likes-text">
+		<span class="likes-count"><?php echo $likes_number ?></span> likes
+	</div>
+</div>
--- wpgrade-content/js/main.js
+++ wpgrade-content/js/main.js
@@ -1003,6 +1003,12 @@ function windowResizeVideos(){
     });
 }
 
+// Like box animation
+$(document).one('click', '.can_like .like-link', function(event){
+    event.preventDefault();
+    $(this).addClass('animate-like');
+});
+
 /* ====== INITIALIZE ====== */
 
 function init() {
