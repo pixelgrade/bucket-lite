fa4f8fc89b2f0ea38f00ce6418d8a6e70c2039ef
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-10-02 16:29:07 +0300

    more translation

--- theme-content/inc/required-plugins/class-tgm-plugin-activation.php
+++ theme-content/inc/required-plugins/class-tgm-plugin-activation.php
@@ -168,18 +168,19 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 				'menu_title'                      => __( 'Install Plugins', wpgrade::textdomain() ),
 				'installing'                      => __( 'Installing Plugin: %s', wpgrade::textdomain() ),
 				'oops'                            => __( 'Something went wrong.', wpgrade::textdomain() ),
-				'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ),
-				'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ),
-				'notice_can_install_update'       => _n_noop( 'There is a new update for %1$s.', 'There are several plugins updates available %1$s.' ),
-				'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ),
-				'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ),
-				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ),
-				'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ),
-				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ),
-				'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ),
-				'install_link' 					  => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
-				'update_link' 					  => _n_noop( 'Begin updating plugin', 'Begin updating plugins' ),
-				'activate_link' 				  => _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
+				'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.',wpgrade::textdomain() ),
+				'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.',wpgrade::textdomain() ),
+				'notice_can_install_update'       => _n_noop( 'There is a new update for %1$s.', 'There are several plugins updates available %1$s.',wpgrade::textdomain() ),
+				'notice_can_install_required_update' => _n_noop( 'This theme requires the following plugin to be updated: %1$s.', 'This theme requires the following plugins to be updated: %1$s.',wpgrade::textdomain() ),
+				'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.',wpgrade::textdomain() ),
+				'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.',wpgrade::textdomain() ),
+				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.',wpgrade::textdomain() ),
+				'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.',wpgrade::textdomain() ),
+				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',wpgrade::textdomain() ),
+				'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.',wpgrade::textdomain() ),
+				'install_link' 					  => _n_noop( 'Begin installing plugin', 'Begin installing plugins',wpgrade::textdomain() ),
+				'update_link' 					  => _n_noop( 'Begin updating plugin', 'Begin updating plugins',wpgrade::textdomain() ),
+				'activate_link' 				  => _n_noop( 'Activate installed plugin', 'Activate installed plugins',wpgrade::textdomain() ),
 				'return'                          => __( 'Return to Required Plugins Installer', wpgrade::textdomain() ),
 				'plugin_activated'                => __( 'Plugin activated successfully.', wpgrade::textdomain() ),
 				'complete'                        => __( 'All plugins installed and activated successfully. %1$s', wpgrade::textdomain() ),
@@ -618,7 +619,7 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 								$update_link_count++; // Increment the install link count
 								if ( current_user_can( 'install_plugins' ) ) {
 									if ( $plugin['required'] )
-										$message['notice_can_install_required'][] = $plugin['name'];
+										$message['notice_can_install_required_update'][] = $plugin['name'];
 									/** This plugin is only recommended */
 									else
 										$message['notice_can_install_update'][] = $plugin['name'];
