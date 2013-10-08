51c80d91a519b78daa24a513cfc8b90bd4ef3a6a
Andrei Lupu <andreilupu>
2013-10-08 17:17:43 +0300

    test ignored files

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         </div><!-- .wrapper -->
     </div><!-- #page -->
 <!-- Google Analytics tracking code -->
-<?php echo wpgrade::option( 'google_analytics', "" ) . "\n"; ?>
-<?php wp_footer(); ?>
+<?php echo wpgrade::option( 'google_analytics' ) . "\n";
+wp_footer(); ?>
 </body>
 </html>
