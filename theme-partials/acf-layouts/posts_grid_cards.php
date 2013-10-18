d0fe7941da02c7501c7a51c51fbe9f7967f7b790
Andrei Lupu <andreilupu>
2013-10-18 15:27:41 +0300

    implemented page builder
    page_builder.php template
    afc-layouts components

---
+++ theme-partials/acf-layouts/posts_grid_cards.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Posts Grid Card Component
+ * Fields available:
+ * @posts_source select (featured / latest / latest_by_cat / latest_by_format / latest_by_reviews)
+ * @number_of_posts number
+ */
+?>
+
+<p>Posts Grid Card</p>
