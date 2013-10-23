577cc3a45a3ce2fbd71aec4bca4ea271f4353075
Razvan Onofrei <razwan.me@gmail.com>
2013-10-23 10:32:09 +0300

    small fixed

--- theme-content/inc/widgets.php
+++ theme-content/inc/widgets.php
@@ -1104,7 +1104,7 @@ class wpgrade_popular_posts extends WP_Widget {
     		<?php
             $index = 0;
     		foreach( $filter_links as $key => $val ) {
-    			echo '<div class="tabs__pane" id="'. $key .'">';
+    			echo '<div class="tabs__pane'. $index++ == 0 ? '' : ' hide'.'" id="'. $key .'">';
     			echo self::showMostViewed( $number, $thumb_size, $key );
     			echo '</div>';
     		} ?>
