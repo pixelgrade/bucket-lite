48279e1e79087d93a6fc2a9d069922ea9238ddfb
Andrei Lupu <andreilupu>
2013-11-01 12:25:20 +0200

    working share-box

--- theme-partials/post-templates/share-box.php
+++ theme-partials/post-templates/share-box.php
@@ -2,11 +2,11 @@
 if ( wpgrade::options('blog_single_show_share_links')) : ?>
 	<div id="share-box">
 		<ul class="nav">
-			<li id="pixlikes" class="share-item">
-				<div class="share-item__icon"><i class="pixcode pixcode--icon icon-e-heart  circle  small"></i></div>
-				<div class="share-item__value">17</div>
-			</li>
-			<?php if ( wpgrade::options('blog_single_share_links_twitter')) { ?>
+			<?php
+			if ( function_exists( 'display_pixlikes' ) ) {
+				display_pixlikes();
+			}
+			if ( wpgrade::options('blog_single_share_links_twitter')) { ?>
 				<li id="twitter" class="share-item" data-url="<?php the_permalink() ?>" data-text="<?php the_excerpt_rss() ?>" data-title="Tweet"></li>
 			<?php }
 			if ( wpgrade::options('blog_single_share_links_facebook')) { ?>
@@ -16,7 +16,7 @@ if ( wpgrade::options('blog_single_show_share_links')) : ?>
 				<li id="gplus" class="share-item" data-url="<?php the_permalink() ?>" data-text="<?php the_excerpt() ?>" data-title="+1"></li>
 			<?php } ?>
 			<li class="share-item share-total">
-				<div class="share-total__value">48</div>
+				<div class="share-total__value">0</div>
 				<div class="share-total__title">Shares</div>
 			</li>
 		</ul>
