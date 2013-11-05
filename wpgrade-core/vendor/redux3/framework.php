4975639f36217b9cb0b313538c080be485ca664a
Andrei Lupu <andreilupu>
2013-11-05 12:25:18 +0200

    update redux

--- wpgrade-core/vendor/redux3/framework.php
+++ wpgrade-core/vendor/redux3/framework.php
@@ -318,7 +318,7 @@ if( !class_exists( 'ReduxFramework' ) ) {
             add_action( 'wp_head', array( &$this, '_enqueue_output' ), 100 );
 
             // Add tracking. PLEASE leave this in tact! It helps us gain needed statistics of uses. Opt-in of course.
-            add_action( 'init', array( &$this, '_tracking' ), 3 );            
+//            add_action( 'init', array( &$this, '_tracking' ), 3 );
 
             // Hook into the WP feeds for downloading exported settings
             add_action( 'do_feed_reduxopts-' . $this->args['opt_name'], array( &$this, '_download_options' ), 1, 1 );
