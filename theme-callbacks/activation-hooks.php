772c63ec6481eb0d93c8dea60b379af2e36d36b0
Andrei Lupu <andreilupu>
2013-11-07 15:15:19 +0200

    ++

--- theme-callbacks/activation-hooks.php
+++ theme-callbacks/activation-hooks.php
@@ -3,6 +3,21 @@
 function wpgrade_callback_geting_active() {
 
 	/**
+	 * make sure pixlikes has the right settings
+	 */
+	$pixlikes_settings = array(
+		'show_on_post' => false,
+		'show_on_page' => false,
+		'show_on_hompage' => false,
+		'show_on_archive' => false,
+		'like_action' => 'click',
+		'hover_time' => 1000,
+		'free_votes' => false,
+		'load_likes_with_ajax' => false,
+	);
+	update_option('pixlikes_settings', $pixlikes_settings);
+
+	/**
 	 * Create custom post types, taxonomies and metaboxes
 	 * These will be taken by pixtypes plugin and converted in their own options
 	 */
