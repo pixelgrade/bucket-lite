f57d860fa1a0bf7c058bb8a0c97dc9bd9426f688
cristianfrumusanu <cristian.frumusanu@gmail.com>
2013-11-11 13:03:47 +0200

    mic fix

--- theme-content/js/main.js
+++ theme-content/js/main.js
@@ -1225,36 +1225,6 @@
 
       this.init();
     };
- 
-    /* MAGNIFIC POPUP INIT */
-
-    function magnificPopupInit(){
-
-      $('.js-post-gallery').each(function() { // the containers for all your galleries should have the class gallery
-          $(this).magnificPopup({
-              delegate: 'a[href$=".jpg"], a[href$=".png"], a[href$=".gif"]', // the container for each your gallery items
-              type: 'image',
-              removalDelay: 500,
-              mainClass: 'mfp-fade',
-              image: {
-                  titleSrc: function (item){
-                      var output = '';
-                      if ( typeof item.el.attr('data-title') !== "undefined" && item.el.attr('data-title') !== "") {
-                        output = item.el.attr('data-title');
-                      }
-                      if ( typeof item.el.attr('data-alt') !== "undefined" && item.el.attr('data-alt') !== "") {
-                        output += '<small>'+item.el.attr('data-alt')+'</small>';
-                      }
-                      return output;
-                  }
-              },
-              gallery:{
-                  enabled:true,
-                  navigateByImgClick: true,
-              }
-          });
-      });      
-    }
       
 
     /* Initialization method
@@ -1484,6 +1454,36 @@
       }
     };
   })(jQuery, window, document);
+
+    /* MAGNIFIC POPUP INIT */
+
+    function magnificPopupInit(){
+
+      $('.js-post-gallery').each(function() { // the containers for all your galleries should have the class gallery
+          $(this).magnificPopup({
+              delegate: 'a[href$=".jpg"], a[href$=".png"], a[href$=".gif"]', // the container for each your gallery items
+              type: 'image',
+              removalDelay: 500,
+              mainClass: 'mfp-fade',
+              image: {
+                  titleSrc: function (item){
+                      var output = '';
+                      if ( typeof item.el.attr('data-title') !== "undefined" && item.el.attr('data-title') !== "") {
+                        output = item.el.attr('data-title');
+                      }
+                      if ( typeof item.el.attr('data-alt') !== "undefined" && item.el.attr('data-alt') !== "") {
+                        output += '<small>'+item.el.attr('data-alt')+'</small>';
+                      }
+                      return output;
+                  }
+              },
+              gallery:{
+                  enabled:true,
+                  navigateByImgClick: true,
+              }
+          });
+      });      
+    }
 
     /* --- $ROYALSLIDER --- */
     var $original_billboard_slider;
