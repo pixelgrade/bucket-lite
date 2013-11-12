aedf3986a3e028b00a50e4e41f94eb753767b839
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-11-12 20:19:16 +0200

    fixed theme options share box

--- theme-partials/post-templates/share-box.php
+++ theme-partials/post-templates/share-box.php
@@ -1,23 +1,23 @@
 <?php
-if ( wpgrade::options('blog_single_show_share_links')) : ?>
+if ( wpgrade::option('blog_single_show_share_links')) : ?>
 	<div id="share-box">
 		<ul class="nav">
 			<?php
 			if ( function_exists( 'display_pixlikes' ) ) {
 				display_pixlikes();
 			}
-			if ( wpgrade::options('blog_single_share_links_twitter')) { ?>
+			if ( wpgrade::option('blog_single_share_links_twitter')) { ?>
 				<li id="twitter" class="share-item" data-url="<?php the_permalink() ?>" data-text="<?php the_excerpt_rss() ?>" data-title="Tweet"></li>
 			<?php }
-			if ( wpgrade::options('blog_single_share_links_facebook')) { ?>
+			if ( wpgrade::option('blog_single_share_links_facebook')) { ?>
 				<li id="facebook" class="share-item" data-url="<?php the_permalink() ?>" data-text="<?php the_excerpt() ?>" data-title="Like"></li>
 			<?php }
-			if ( wpgrade::options('blog_single_share_links_googleplus')) {?>
+			if ( wpgrade::option('blog_single_share_links_googleplus')) {?>
 				<li id="gplus" class="share-item" data-url="<?php the_permalink() ?>" data-text="<?php the_excerpt() ?>" data-title="+1"></li>
 			<?php } ?>
 			<li class="share-item share-total">
 				<div class="share-total__value">0</div>
-				<div class="share-total__title">Shares</div>
+				<div class="share-total__title"><?php _e('Shares', wpgrade::textdomain()); ?></div>
 			</li>
 		</ul>
 	</div>
