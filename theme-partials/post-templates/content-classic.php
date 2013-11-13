797605c1d9c3e84275da978ab6875104ca45dac1
George Olaru <george.olaru@pixelgrade.com>
2013-11-13 14:52:24 +0200

    post classic styling + markup

--- theme-partials/post-templates/content-classic.php
+++ theme-partials/post-templates/content-classic.php
@@ -3,33 +3,40 @@
  * Template to display the article in archives in a clasic way
  */
 ?>
-<article <?php post_class('article  article--thumb media flush--bottom'); ?>>
-	<div class="media__img--rev  four-twelfths">
+<article <?php post_class('article  article--thumb media flush--bottom grid'); ?>>
+	<div class="media__img--rev grid__item five-twelfths palm-one-whole">
 		<?php
 		if (has_post_thumbnail()):
-			$image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'post-small');
+			$image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'post-medium');
 			$image_ratio = $image[2] * 100/$image[1]; ?>
 			<a href="<?php the_permalink(); ?>" class="image-wrap" style="padding-top: <?php echo $image_ratio; ?>%">
 				<img src="<?php echo $image[0] ?>" />
 			</a>
 		<?php endif; ?>
 	</div>
-	<div class="media__body">
+	<div class="media__body grid__item seven-twelfths palm-one-whole">
 		<?php
 		$categories = get_the_category();
 		if ($categories) {
-			$category = $categories[0];
-			echo '<div class="article__category">
-                                            <a class="small-link" href="'. get_category_link($category->term_id) .'" title="'. esc_attr(sprintf(__("View all posts in %s", wpgrade::textdomain()), $category->name)) .'">'. $category->cat_name.'</a>
-                                          </div>';
+			echo '<div class="article__category">';
+			foreach ($categories as $category):
+	                    echo '<a class="small-link" href="'. get_category_link($category->term_id) .'" title="'. esc_attr(sprintf(__("View all posts in %s", wpgrade::textdomain()), $category->name)) .'">'. $category->cat_name.'</a>';
+            endforeach;
+	        echo '</div>';
 		} ?>
 		<div class="article__title  article--thumb__title">
 			<a href="<?php the_permalink(); ?>"><h3 class="hN"><?php the_title(); ?></h3></a>
 		</div>
-		<ul class="nav  article__meta-links">
+		<div class="article--grid__body">
+	        <div class="article__content">
+	            <?php  the_excerpt(); ?>
+	        </div>
+	    </div>
+	    <ul class="nav  article__meta-links">
 			<li><i class="icon-time"></i> <?php the_time('j M') ?></li>
 			<li><i class="icon-comment"></i>  <?php comments_number('0', '1', '%'); ?></li>
 			<li><i class="icon-heart"></i> <?php if ( function_exists('get_pixlikes') ) {echo get_pixlikes(wpgrade::lang_original_post_id(get_the_ID()));} ?></li>
 		</ul>
 	</div>
 </article>
+<hr class="separator  separator--subsection">
