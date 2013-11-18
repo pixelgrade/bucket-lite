ef340def9668d1e8080c574d335f0d28a53437bc
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-11-18 12:59:58 +0200

    comments

--- theme-utilities/includes/functions/template-tags.php
+++ theme-utilities/includes/functions/template-tags.php
@@ -8,62 +8,6 @@
  * @since wpGrade 1.0
  */
 
-if ( ! function_exists( 'wpgrade_comment' ) ) :
-/**
- * Template for comments and pingbacks.
- *
- * Used as a callback by wp_list_comments() for displaying the comments.
- *
- * @since wpGrade 1.0
- */
-function wpgrade_comment( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment;
-	switch ( $comment->comment_type ) :
-		case 'pingback' :
-		case 'trackback' :
-	?>
-	<li class="post pingback">
-		<p><?php _e( 'Pingback:', wpgrade::textdomain() ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', wpgrade::textdomain() ), ' ' ); ?></p>
-	<?php
-			break;
-		default :
-	?>
-	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<article id="comment-<?php comment_ID(); ?>">
-			<footer>
-				<div class="comment-author vcard">
-					<?php //echo get_avatar( $comment, 40 ); ?>
-					<?php printf( __( '%s', wpgrade::textdomain() ), sprintf( '<span class="fn">%s</span>', get_comment_author_link() ) ); ?>
-					<a class="comment-date" href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>"><?php
-						/* translators: 1: date, 2: time */
-						the_time('j M y');
-					?></time></a>
-				</div><!-- .comment-author .vcard -->
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em><?php _e( 'Your comment is awaiting moderation.', wpgrade::textdomain() ); ?></em>
-					<br />
-				<?php endif; ?>
-
-			</footer>
-
-			<div class="comment-content"><?php comment_text(); ?></div>
-
-			<p class="comment-meta commentmetadata">
-				<?php edit_comment_link( __( 'Edit', wpgrade::textdomain() ), ' ' );
-				?>
-			</p> <!-- .comment-meta .commentmetadata -->
-
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
-		</article><!-- #comment-## -->
-
-	<?php
-			break;
-	endswitch;
-}
-endif; // ends check for wpgrade_comment()
-
 /**
  * Returns true if a blog has more than 1 category
  *
