553235eb583359e5c104fd3b3ca5dd7dfc22b536
George Olaru <george.olaru@pixelgrade.com>
2013-12-05 14:20:25 +0200

    back-to-top

--- footer.php
+++ footer.php
@@ -84,6 +84,11 @@
                     </a>
                 </li>
             </ul>
+            
+            <?php if (wpgrade::option('posts_stats')): ?>
+            <div class="back-to-top"><a href="#">Back to Top</a></div>
+            <?php endif; ?>
+            
         </div>
     </div>
     <?php endif; ?>
@@ -112,10 +117,6 @@
             <div class="footer__sidebar">
 
                 <div class="container">
-
-                    <?php if (wpgrade::option('posts_stats')): ?>
-                    <div class="back-to-top"><a href="#">Back to Top</a></div>
-                    <?php endif; ?>
                     
                     <div class="footer__widget-area  grid"><!--
                         <?php if ( is_active_sidebar( 'sidebar-footer-first-1' ) ) : ?>
