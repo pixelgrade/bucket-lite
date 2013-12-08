ccaf7eab40be3a2acce55b0f5a5d5036bb76d786
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-12-08 10:48:03 +0200

    test

--- theme-content/js/main.js
+++ theme-content/js/main.js
@@ -144,8 +144,51 @@
 
     // --- MODIFIED
     // https://github.com/CSS-Tricks/jQuery-Organic-Tabs
-    $.organicTabs=function(el,options){var base=this;base.$el=$(el);base.$nav=base.$el.find(".tabs__nav");base.init=function(){base.options=$.extend({},$.organicTabs.defaultOptions,options);var $allListWrap=base.$el.find(".tabs__content");var curList=base.$el.find("a.current").attr("href").substring(1);$allListWrap.height(base.$el.find("#"+curList).height());base.$nav.delegate("li > a","click",function(){var curList=base.$el.find("a.current").attr("href").substring(1),$newList=$(this),listID=$newList.attr("href").substring(1);if((listID!=curList)&&(base.$el.find(":animated").length==0)){base.$el.find("#"+curList).css({opacity:0,"z-index":10});var newHeight=base.$el.find("#"+listID).height();$allListWrap.css({height:newHeight});setTimeout(function(){base.$el.find("#"+curList);base.$el.find("#"+listID).css({opacity:1,"z-index":20});base.$el.find(".tabs__nav li a").removeClass("current");$newList.addClass("current");},250);}return false;});};base.init();};$.organicTabs.defaultOptions={speed:300};$.fn.organicTabs=function(options){return this.each(function(){(new $.organicTabs(this,options));});};
-
+    $.organicTabs = function (el, options) {
+    var base = this;
+    base.$el = $(el);
+    base.$nav = base.$el.find(".tabs__nav");
+    base.init = function () {
+        base.options = $.extend({}, $.organicTabs.defaultOptions, options);
+        var $allListWrap = base.$el.find(".tabs__content");
+        var curList = base.$el.find("a.current").attr("href").substring(1);
+        $allListWrap.height(base.$el.find("#" + curList).height());
+        base.$nav.delegate("li > a", "click", function () {
+            var curList = base.$el.find("a.current").attr("href").substring(1),
+                $newList = $(this),
+                listID = $newList.attr("href").substring(1);
+            if ((listID != curList) && (base.$el.find(":animated").length == 0)) {
+                base.$el.find("#" + curList).css({
+                    opacity: 0,
+                    "z-index": 10
+                });
+                var newHeight = base.$el.find("#" + listID).height();
+                $allListWrap.css({
+                    height: newHeight
+                });
+                setTimeout(function () {
+                    base.$el.find("#" + curList);
+                    base.$el.find("#" + listID).css({
+                        opacity: 1,
+                        "z-index": 20
+                    });
+                    base.$el.find(".tabs__nav li a").removeClass("current");
+                    $newList.addClass("current");
+                }, 250);
+            }
+            return false;
+        });
+    };
+    base.init();
+};
+$.organicTabs.defaultOptions = {
+    speed: 300
+};
+$.fn.organicTabs = function (options) {
+    return this.each(function () {
+        (new $.organicTabs(this, options));
+    });
+};
     
     /* --- FASTCLICK --- */
 
