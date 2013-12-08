8393454416d2de27bb867cc06809b2af831e930f
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-12-08 11:11:52 +0200

    test

--- theme-content/js/main.js
+++ theme-content/js/main.js
@@ -144,51 +144,80 @@
 
     // --- MODIFIED
     // https://github.com/CSS-Tricks/jQuery-Organic-Tabs
-    $.organicTabs = function (el, options) {
-    var base = this;
-    base.$el = $(el);
-    base.$nav = base.$el.find(".tabs__nav");
-    base.init = function () {
-        base.options = $.extend({}, $.organicTabs.defaultOptions, options);
-        var $allListWrap = base.$el.find(".tabs__content");
-        var curList = base.$el.find("a.current").attr("href").substring(1);
-        $allListWrap.height(base.$el.find("#" + curList).height());
-        base.$nav.delegate("li > a", "click", function () {
-            var curList = base.$el.find("a.current").attr("href").substring(1),
-                $newList = $(this),
-                listID = $newList.attr("href").substring(1);
-            if ((listID != curList) && (base.$el.find(":animated").length == 0)) {
-                base.$el.find("#" + curList).css({
-                    opacity: 0,
-                    "z-index": 10
-                });
-                var newHeight = base.$el.find("#" + listID).height();
-                $allListWrap.css({
-                    height: newHeight
-                });
-                setTimeout(function () {
-                    base.$el.find("#" + curList);
-                    base.$el.find("#" + listID).css({
-                        opacity: 1,
-                        "z-index": 20
+    $.organicTabs = function(el, options) {
+    
+        var base = this;
+        base.$el = $(el);
+        base.$nav = base.$el.find(".nav");
+                
+        base.init = function() {
+        
+            base.options = $.extend({},$.organicTabs.defaultOptions, options);
+            
+            // Accessible hiding fix
+            $(".hide").css({
+                "position": "relative",
+                "top": 0,
+                "left": 0,
+                "display": "none"
+            }); 
+            
+            base.$nav.delegate("li > a", "click", function() {
+            
+                // Figure out current list via CSS class
+                var curList = base.$el.find("a.current").attr("href").substring(1),
+                
+                // List moving to
+                    $newList = $(this),
+                    
+                // Figure out ID of new list
+                    listID = $newList.attr("href").substring(1),
+                
+                // Set outer wrapper height to (static) height of current inner list
+                    $allListWrap = base.$el.find(".list-wrap"),
+                    curListHeight = $allListWrap.height();
+                $allListWrap.height(curListHeight);
+                                        
+                if ((listID != curList) && ( base.$el.find(":animated").length == 0)) {
+                                            
+                    // Fade out current list
+                    base.$el.find("#"+curList).fadeOut(base.options.speed, function() {
+                        
+                        // Fade in new list on callback
+                        base.$el.find("#"+listID).fadeIn(base.options.speed);
+                        
+                        // Adjust outer wrapper to fit new list snuggly
+                        var newHeight = base.$el.find("#"+listID).height();
+                        $allListWrap.animate({
+                            height: newHeight
+                        });
+                        
+                        // Remove highlighting - Add to just-clicked tab
+                        base.$el.find(".nav li a").removeClass("current");
+                        $newList.addClass("current");
+                            
                     });
-                    base.$el.find(".tabs__nav li a").removeClass("current");
-                    $newList.addClass("current");
-                }, 250);
-            }
-            return false;
+                    
+                }   
+                
+                // Don't behave like a regular link
+                // Stop propegation and bubbling
+                return false;
+            });
+            
+        };
+        base.init();
+    };
+    
+    $.organicTabs.defaultOptions = {
+        "speed": 300
+    };
+    
+    $.fn.organicTabs = function(options) {
+        return this.each(function() {
+            (new $.organicTabs(this, options));
         });
     };
-    base.init();
-};
-$.organicTabs.defaultOptions = {
-    speed: 300
-};
-$.fn.organicTabs = function (options) {
-    return this.each(function () {
-        (new $.organicTabs(this, options));
-    });
-};
     
     /* --- FASTCLICK --- */
 
