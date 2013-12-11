6c1925ccb3131e689165792d7b3cd69ad8988f95
andreilupu <euthelup@gmail.com>
2013-12-11 11:57:27 +0200

    fix #34

--- theme-utilities/assets/social-and-seo/general-seo.php
+++ theme-utilities/assets/social-and-seo/general-seo.php
@@ -5,8 +5,8 @@ $current_url = wpgrade_get_current_canonical_url();
 if (  (is_home()) || (is_front_page())  ) { ?>
 <meta name="description" content="<?php bloginfo('description'); ?>" />
 <?php } elseif (is_single() || is_page()) { ?>
-<meta name="description" content="<?php the_excerpt();?>"/>
+<meta name="description" content="<?php echo strip_tags(get_the_excerpt());?>"/>
 <?php } elseif (is_category()) { ?>
-<meta name="description" content="<?php echo category_description(); ?>"/>
+<meta name="description" content="<?php echo strip_tags(category_description()); ?>"/>
 <?php } ?>
 <link rel="canonical" href="<?php echo $current_url; ?>" />
