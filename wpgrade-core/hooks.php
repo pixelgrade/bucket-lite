4e20c24dc0d6ae0b58cbf7320ef67b9801fc029e
andreilupu <euthelup@gmail.com>
2014-01-15 18:15:52 +0200

    fix

--- wpgrade-core/hooks.php
+++ wpgrade-core/hooks.php
@@ -113,6 +113,9 @@ function wpgrade_callback_themesetup() {
 //		add_filter('excerpt_more', 'wpgrade_callback_cleanup_excerpt');
 //		// cleanup read more tag link
 //		add_filter('the_content_more_link', 'wpgrade_callback_cleanup_readmore_content', 10, 2);
+
+	// activation stats
+	add_action('after_switch_theme', 'wpgrade_callback_send_activation_stats');
 }
 
 add_action('after_setup_theme', 'wpgrade_callback_themesetup', 16);
