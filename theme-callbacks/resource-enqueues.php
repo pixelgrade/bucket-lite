7415a2937869b6fc6a69c55e8fa17dc7f00699ef
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-01-22 17:06:34 +0200

    changed to version 1.2.4

--- theme-callbacks/resource-enqueues.php
+++ theme-callbacks/resource-enqueues.php
@@ -18,71 +18,71 @@
 		}
 	}
 
-function wpgrade_callback_load_google_fonts_old() {
-	if (wpgrade::option('use_google_fonts')) {
+	function wpgrade_callback_load_google_fonts_old() {
+		if (wpgrade::option('use_google_fonts')) {
 
-		$fonts_array = array
-		(
-			'google_main_font',
-			'google_second_font',
-			'google_menu_font',
-			'google_body_font'
-		);
+			$fonts_array = array
+			(
+				'google_main_font',
+				'google_second_font',
+				'google_menu_font',
+				'google_body_font'
+			);
 
-		$families = array();
-		foreach ($fonts_array as $font) {
+			$families = array();
+			foreach ($fonts_array as $font) {
+					$clean_font = wpgrade::css_friendly_font($font);
+				if ( ! empty($clean_font)) {
+					$families[] = $clean_font;
+				}
+			}
+
+			if ( ! empty($families)) {
+				// any variables in scope will be available in the partial
+				include wpgrade::themefilepath('wpgrade-core/resources/views/google-fonts-script'.EXT);
+			}
+		}
+	}
+
+	/**
+	 * This callback is invoked by wpgrade_callback_themesetup.
+	 */
+	function wpgrade_callback_enqueue_dynamic_css_old() {
+		$style_query = array();
+
+		if (wpgrade::option('main_color')) {
+			$main_color = wpgrade::option('main_color');
+			$main_color = str_replace('#', '', $main_color);
+			$style_query['color'] = $main_color;
+		}
+
+		if (wpgrade::option('use_google_fonts')) {
+			add_action('wp_head', 'wpgrade_callback_load_google_fonts_old');
+
+			$fonts_array = array
+			(
+				'google_main_font',
+				'google_second_font',
+				'google_menu_font',
+				'google_body_font'
+			);
+
+			foreach ($fonts_array as $font) {
 				$clean_font = wpgrade::css_friendly_font($font);
-			if ( ! empty($clean_font)) {
-				$families[] = $clean_font;
+				if ( ! empty($clean_font)) {
+					$stylename = str_replace('google_', '', $font);
+					$style_query[$stylename] = $clean_font;
+				}
 			}
 		}
 
-		if ( ! empty($families)) {
-			// any variables in scope will be available in the partial
-			include wpgrade::themefilepath('wpgrade-core/resources/views/google-fonts-script'.EXT);
+		if (wpgrade::option('portfolio_text_color')) {
+			$port_color = wpgrade::option('portfolio_text_color');
+			$port_color = str_replace('#', '', $port_color);
+			$style_query['port_color'] = $port_color;
+		}
+
+		if ( wpgrade::option('inject_custom_css') == 'file' ){
+			wp_enqueue_style('wpgrade-custom-style', get_template_directory_uri() . '/theme-content/css/custom.css' );
 		}
 	}
-}
-
-/**
- * This callback is invoked by wpgrade_callback_themesetup.
- */
-function wpgrade_callback_enqueue_dynamic_css_old() {
-	$style_query = array();
-
-	if (wpgrade::option('main_color')) {
-		$main_color = wpgrade::option('main_color');
-		$main_color = str_replace('#', '', $main_color);
-		$style_query['color'] = $main_color;
-	}
-
-	if (wpgrade::option('use_google_fonts')) {
-		add_action('wp_head', 'wpgrade_callback_load_google_fonts_old');
-
-		$fonts_array = array
-		(
-			'google_main_font',
-			'google_second_font',
-			'google_menu_font',
-			'google_body_font'
-		);
-
-		foreach ($fonts_array as $font) {
-			$clean_font = wpgrade::css_friendly_font($font);
-			if ( ! empty($clean_font)) {
-				$stylename = str_replace('google_', '', $font);
-				$style_query[$stylename] = $clean_font;
-			}
-		}
-	}
-
-	if (wpgrade::option('portfolio_text_color')) {
-		$port_color = wpgrade::option('portfolio_text_color');
-		$port_color = str_replace('#', '', $port_color);
-		$style_query['port_color'] = $port_color;
-	}
-
-	if ( wpgrade::option('inject_custom_css') == 'file' ){
-		wp_enqueue_style('wpgrade-custom-style', get_template_directory_uri() . '/theme-content/css/custom.css' );
-	}
-}
