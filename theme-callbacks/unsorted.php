a02eebffcad6666c1d2e4c0b43eec2146e80aa0b
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-01-22 16:37:49 +0200

    better excerpt and solved #61

--- theme-callbacks/unsorted.php
+++ theme-callbacks/unsorted.php
@@ -35,14 +35,10 @@ function wpgrade_contains_any_multibyte($string)
 //@todo CLEANUP refactor function
 function wpgrade_better_excerpt($text = '') {
 	global $post;
-
-	if (empty($text)) {
-		//need to grab the content
-		$text = get_the_content();
-	}
+	$raw_excerpt = '';
 
 	//if the post has a manual excerpt ignore the content given
-	if (function_exists('has_excerpt') && has_excerpt()) {
+	if ($text == '' && function_exists('has_excerpt') && has_excerpt()) {
 		$text = get_the_excerpt();
 		$raw_excerpt = $text;
 
@@ -56,10 +52,16 @@ function wpgrade_better_excerpt($text = '') {
 		// Enable formatting in excerpts - Add HTML tags that you want to be parsed in excerpts
 		$allowed_tags = '<p><a><em><strong><i><br><h1><h2><h3><h4><h5><h6><blockquote><ul><li><ol>';
 		$text = strip_tags($text, $allowed_tags);
-		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
-		$text .= $excerpt_more;
-	}
-	else {
+//		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+//		$text .= $excerpt_more;
+		
+	} else {
+		
+		if (empty($text)) {
+			//need to grab the content
+			$text = get_the_content();
+		}
+		
 		$raw_excerpt = $text;
 		$text = strip_shortcodes( $text );
 		$text = apply_filters('the_content', $text);
@@ -82,18 +84,15 @@ function wpgrade_better_excerpt($text = '') {
 		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
 		
 		//test if we are dealing with a utf8 text - like chinese
-		if (wpgrade_contains_any_multibyte($text)) {
+		if (wpgrade_is_all_multibyte($text)) {
 			//then we simply split my mb characters rather than words
-			$text = short_text($text,$excerpt_length,excerpt_length);
-			$text = force_balance_tags( $text );
+			$text = short_text($text,$excerpt_length,$excerpt_length);
 			$text = $text . $excerpt_more;
 		} else {
 			$words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
-		
 			if ( count($words) > $excerpt_length ) {
 				array_pop($words);
 				$text = implode(' ', $words);
-				$text = force_balance_tags( $text );
 				$text = $text . $excerpt_more;
 			} else {
 				$text = implode(' ', $words);
@@ -182,11 +181,14 @@ function wpgrade_comments($comment, $args, $depth) {
      * @param  [int] $limit      [description]
      * @return [type]             [description]
      */
-    function short_text($text, $cut_length, $limit){
+    function short_text($text, $cut_length, $limit, $echo = true){
 		$char_count = mb_strlen($text);
         $text = ( $char_count > $limit ) ? mb_substr($text,0,$cut_length).wpgrade::option('blog_excerpt_more_text') : $text;
-
-        echo $text;
+		if ($echo) {
+			echo $text;
+		} else {
+			return $text;
+		}
     }
 
 function custom_excerpt_length( $length ) {
