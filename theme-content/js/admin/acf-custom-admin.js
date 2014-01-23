df63ce96b9276bfbe64651f4432bfd097220c046
andreilupu <euthelup@gmail.com>
2014-01-23 14:24:40 +0200

    acf fields fold which closes #75

--- theme-content/js/admin/acf-custom-admin.js
+++ theme-content/js/admin/acf-custom-admin.js
@@ -1,55 +1,51 @@
 ;(function($){
 	$(document).ready(function(){
 
-		// custom show/hide for fields which depends on posts_source value
-		$('td[data-field_name=posts_source_category]').parent().hide();
-		$('td[data-field_name=posts_source_post_formats]').parent().hide();
+        var processHiddenFiles = function(){
 
-		$('td[data-field_name="posts_source"]').each(function(i,e){
-			$parent = $(this).parent();
-			$select = $(this).find('select');
-			if ( $select.val() == 'latest_by_cat' ) {
-				$parent.siblings().find('td[data-field_name=posts_source_category]').parent().show();
-			}
+            // custom show/hide for fields which depends on posts_source value
+            $('tr[data-field_name=posts_source_category]').hide();
+            $('tr[data-field_name=posts_source_post_formats]').hide();
 
-			if ( $select.val() == 'latest_by_format' ) {
-				$parent.siblings().find('td[data-field_name=posts_source_post_formats]').parent().show();
-			}
-		});
+
+            $('tr[data-field_name="posts_source"]').each(function(i,e){
+
+
+                var $parent = $(this).parent();
+                var $select = $(this).find('select');
+
+                if ( $select.val() == 'latest_by_cat' ) {
+                    $(this).siblings('tr[data-field_name=posts_source_category]').show();
+                }
+
+                if ( $select.val() == 'latest_by_format' ) {
+                    $(this).siblings('tr[data-field_name=posts_source_post_formats]').show();
+                }
+            });
+
+        }
+
+        processHiddenFiles();
+
+//		$('td[data-field_name="posts_source"]').each(function(i,e){
+//			$parent = $(this).parent();
+//			$select = $(this).find('select');
+//			if ( $select.val() == 'latest_by_cat' ) {
+//				$parent.siblings().find('td[data-field_name=posts_source_category]').parent().show();
+//			}
+//
+//			if ( $select.val() == 'latest_by_format' ) {
+//				$parent.siblings().find('td[data-field_name=posts_source_post_formats]').parent().show();
+//			}
+//		});
 		// all this above should be also moved in the "on change" action below
-		$(document).on('change', 'td[data-field_name=posts_source]', function(){
-			$('td[data-field_name=posts_source_category]').parent().hide();
-			$('td[data-field_name=posts_source_post_formats]').parent().hide();
-
-			$('td[data-field_name="posts_source"]').each(function(i,e){
-				$parent = $(this).parent();
-				$select = $(this).find('select');
-				if ( $select.val() == 'latest_by_cat' ) {
-					$parent.siblings().find('td[data-field_name=posts_source_category]').parent().show();
-				}
-
-				if ( $select.val() == 'latest_by_format' ) {
-					$parent.siblings().find('td[data-field_name=posts_source_post_formats]').parent().show();
-				}
-			});
+		$(document).on('change', 'tr[data-field_name=posts_source] select', function(){
+            processHiddenFiles();
 		});
 
 		$(document).on('acf/setup_fields', function(){
 
-			$('td[data-field_name=posts_source_category]').parent().hide();
-			$('td[data-field_name=posts_source_post_formats]').parent().hide();
-
-			$('td[data-field_name="posts_source"]').each(function(i,e){
-				$parent = $(this).parent();
-				$select = $(this).find('select');
-				if ( $select.val() == 'latest_by_cat' ) {
-					$parent.siblings().find('td[data-field_name=posts_source_category]').parent().show();
-				}
-
-				if ( $select.val() == 'latest_by_format' ) {
-					$parent.siblings().find('td[data-field_name=posts_source_post_formats]').parent().show();
-				}
-			});
+            processHiddenFiles();
 		});
 
 	});
