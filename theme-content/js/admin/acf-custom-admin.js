8de32cd7969470f11a17e7996a9c25bcfcf21fa2
andreilupu <euthelup@gmail.com>
2014-01-23 14:26:46 +0200

    ++

--- theme-content/js/admin/acf-custom-admin.js
+++ theme-content/js/admin/acf-custom-admin.js
@@ -27,24 +27,12 @@
 
         processHiddenFiles();
 
-//		$('td[data-field_name="posts_source"]').each(function(i,e){
-//			$parent = $(this).parent();
-//			$select = $(this).find('select');
-//			if ( $select.val() == 'latest_by_cat' ) {
-//				$parent.siblings().find('td[data-field_name=posts_source_category]').parent().show();
-//			}
-//
-//			if ( $select.val() == 'latest_by_format' ) {
-//				$parent.siblings().find('td[data-field_name=posts_source_post_formats]').parent().show();
-//			}
-//		});
 		// all this above should be also moved in the "on change" action below
 		$(document).on('change', 'tr[data-field_name=posts_source] select', function(){
             processHiddenFiles();
 		});
 
 		$(document).on('acf/setup_fields', function(){
-
             processHiddenFiles();
 		});
 
