d6d58ecf272440339f206aa15e713545f10f5c87
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-02-24 18:40:18 +0200

    shop + single product

--- woocommerce/single-product/sale-flash.php
+++ woocommerce/single-product/sale-flash.php
@@ -13,6 +13,6 @@ global $post, $product;
 ?>
 <?php if ( $product->is_on_sale() ) : ?>
 
-	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . __( 'Sale!', 'woocommerce' ) . '</span>', $post, $product ); ?>
+	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="badge  badge--article  badge--product  badge--product-single  badge--sale">' . __( 'Sale!', 'woocommerce' ) . '</span>', $post, $product ); ?>
 
 <?php endif; ?>
