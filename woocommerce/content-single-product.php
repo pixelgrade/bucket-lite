0cabce29015b9b43f3a6fed5b9432ffe6043c289
andreilupu <euthelup@gmail.com>
2014-02-25 12:02:37 +0200

    related products full width

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -38,35 +38,38 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		do_action( 'woocommerce_before_single_product_summary' );
 	?>
 	--><div class="grid__item  one-whole  lap-and-up-one-half  product-info">
-	<div class="summary entry-summary">
+		<div class="summary entry-summary">
 
-		<?php
-			/**
-			 * woocommerce_single_product_summary hook
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
-		<hr class="separator  separator--striped" />
-	</div><!-- .summary -->
+			<?php
+				/**
+				 * woocommerce_single_product_summary hook
+				 *
+				 * @hooked woocommerce_template_single_title - 5
+				 * @hooked woocommerce_template_single_price - 10
+				 * @hooked woocommerce_template_single_excerpt - 20
+				 * @hooked woocommerce_template_single_add_to_cart - 30
+				 * @hooked woocommerce_template_single_meta - 40
+				 * @hooked woocommerce_template_single_sharing - 50
+				 */
+				do_action( 'woocommerce_single_product_summary' );
+			?>
+
+		</div><!-- .summary -->
+	</div>
+
+	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+	<hr class="separator  separator--striped" />
 
 	<?php
-		/**
-		 * woocommerce_after_single_product_summary hook
-		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_output_related_products - 20
-		 */
-		do_action( 'woocommerce_after_single_product_summary' );
+	/**
+	 * woocommerce_after_single_product_summary hook
+	 *
+	 * @hooked woocommerce_output_product_data_tabs - 10
+	 * @hooked woocommerce_output_related_products - 20
+	 */
+	do_action( 'woocommerce_after_single_product_summary' );
 	?>
-	</div>
-	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+
 </div><!-- #product-<?php the_ID(); ?> -->
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
