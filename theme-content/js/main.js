d561aa6de32c4f5c6e45db2c8bb8b62dd5d7af42
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-04-28 12:45:34 +0300

    fixes #149, fixes #150

--- theme-content/js/main.js
+++ theme-content/js/main.js
@@ -1678,23 +1678,23 @@ a._i7:a.slider}),a.ev.on("rsAfterSizePropSet",function(){var b,c=a.st.visibleNea
 	function riloadrInit() {
 		// Lazy loading for images with '.lazy' class
 		var riloadrImages = new Riloadr({
-                    name : 'lazy',
-                    breakpoints: [
-                        {name: 'whatever' , minWidth: 1}
-                    ],
-                    defer: {
-                        mode: 'load'
-                    }
+            name : 'lazy',
+            breakpoints: [
+                {name: 'whatever' , minWidth: 1}
+            ],
+            defer: {
+                mode: 'load'
+            }
 		});        
 
 		// Responsive Featured Image for single post page
 		var riloadrSingle = new Riloadr({
-                    name : 'riloadr-single',
-                    breakpoints: [
-                        {name: 'small' , maxWidth: 400},
-                        {name: 'big'   , minWidth: 401}
-                    ],
-                    watchViewportWidth: "*"
+            name : 'riloadr-single',
+            breakpoints: [
+                {name: 'small' , maxWidth: 400},
+                {name: 'big'   , minWidth: 401}
+            ],
+            watchViewportWidth: "*"
 		});
 	}
 
