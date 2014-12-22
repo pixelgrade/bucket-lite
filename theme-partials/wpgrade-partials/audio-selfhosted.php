9af99eabfa4f9b3df991162afea0af7d179a4069
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-12-22 17:19:51 +0200

    improved audio post format

--- theme-partials/wpgrade-partials/audio-selfhosted.php
+++ theme-partials/wpgrade-partials/audio-selfhosted.php
@@ -7,14 +7,15 @@ if ( ! empty( $audio_poster ) ): ?>
 
 $mp3_attr = '';
 $ogg_attr = '';
-
+$m4a_attr = '';
 if(!empty($audio_mp3)) {
 	$mp3_attr = 'mp3="'.$audio_mp3 .'"';
 }
-
 if(!empty($audio_ogg)) {
 	$ogg_attr = 'ogg="'.$audio_ogg .'"';
 }
+if(!empty($audio_m4a)) {
+	$m4a_attr = 'ogg="'.$audio_m4a .'"';
+}
 
-echo(do_shortcode('[audio '.$mp3_attr.' '.$ogg_attr.'][/audio]'));
-
+echo(do_shortcode('[audio '.$mp3_attr.' '.$ogg_attr.' '.$m4a_attr.'][/audio]'));
