936b9be8a912dbb77dac45f96970538000eb1846
andreilupu <andreilupu@pixelgrade.com>
2015-06-26 18:03:29 +0300

    fix social icons list when people go full retard

--- theme-partials/wpgrade-partials/social-icons-list.php
+++ theme-partials/wpgrade-partials/social-icons-list.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $social_links = wpgrade::option('social_icons');
 
 $target = '';
@@ -6,15 +6,15 @@ if ( wpgrade::option('social_icons_target_blank') ) {
 	$target = 'target="_blank"';
 }
 
-if (!empty($social_links)):
-	foreach ($social_links as $domain => $icon):
-		if (isset($icon['value'] ) && isset($icon['checkboxes']['header'] ) ): $value = $icon['value']; ?>
-		    <li>
-		        <a class="social-icon-link" href="<?php echo $value ?>" <?php echo $target ?>>
-		            <i class="pixcode  pixcode--icon  icon-e-<?php echo $domain; ?> square"></i>
-		        </a>
-		    </li>
-		<?php endif;
-	endforeach;
-endif;
-
+if ( ! empty( $social_links ) ) {
+	foreach ($social_links as $domain => $icon) {
+		if ( isset( $icon['value'] ) && ! empty( $icon['value'] ) && isset( $icon['checkboxes']['header'] ) ) {
+			$value = $icon['value']; ?>
+			<li>
+				<a class="social-icon-link" href="<?php echo $value ?>" <?php echo $target ?>>
+					<i class="pixcode  pixcode--icon  icon-e-<?php echo $domain; ?> square"></i>
+				</a>
+			</li>
+		<?php }
+	}
+}
