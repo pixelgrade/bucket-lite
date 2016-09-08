e5d52ae54ccb2a49be0e0c759a35f5b2bac8356a
Cristian Frumusanu <cristian.frumusanu@pixelgrade.com>
2016-09-08 13:26:09 +0300

    Updated WooCommerce files

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -12,7 +12,7 @@
  * @see     http://docs.woothemes.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.5.0
+ * @version 2.6.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
