fb9ef0950622c8f29eaf11803f4089522e15d7cc
George <george.olaru@pixelgrade.com>
2017-04-26 15:44:14 +0300

    Added PixCare Integration #256

---
+++ inc/integrations.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Require files that deal with various plugin integrations.
+*
+* @package Bucket
+*/
+
+/**
+ * Load PixCare compatibility file.
+ */
+require get_template_directory() . '/inc/integrations/pixcare.php';
