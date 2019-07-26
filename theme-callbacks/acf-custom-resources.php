431e1fe65efb2793613309b38a788052345d0296
madalingorbanescu <madalin@pixelgrade.com>
2019-07-26 16:03:44 +0300

    Remove more ACF related files

--- theme-callbacks/acf-custom-resources.php
+++ theme-callbacks/acf-custom-resources.php
@@ -1,16 +1,5 @@
 <?php
 
-add_action( 'acf/input/admin_enqueue_scripts', 'wpgrade_callback_add_acf_custom_resources');
-
-function wpgrade_callback_add_acf_custom_resources (){
-
-	// custom css
-	wp_enqueue_style('wpgrade-acf-custom', wpgrade::resourceuri('css/admin/acf-custom-admin.css') );
-
-	// custom js
-	wp_enqueue_script('wpgrade-acf-custom-script', wpgrade::resourceuri('js/admin/acf-custom-admin.js'), array( 'jquery' ) );
-}
-
 /*
  * A warning message when the user try to activate it's own ACF.
  * We cannot allow that since we need our own plugin version with our add-ons.
